0fd3ee1d6e03d2ee1f608b1ca1ed36428ec9a173
Byung-wan Kwak <59753636+wani-kwak@users.noreply.github.com>
2020-08-23 17:16:39 +0900

    1

--- Project1/Project1/소스.cpp
+++ Project1/Project1/소스.cpp
@@ -1,9 +1,31 @@
 #include<iostream>
-
 using namespace std;
 
+struct ABCD {
+	int x;
+	int y;
+	int z;
+};
+
 int main() {
-	for (int i = -5; i <= 5; i++) {
-		cout << i << " ";
-	}
+	ABCD t, h, k;
+	t.x = 3;
+	t.y = 4;
+	t.z = 2;
+
+	h.x = 1;
+	h.y = 5;
+	h.z = 7;
+
+	k.x = t.x + h.x;
+	k.y = t.y + h.y;
+	k.z = t.z + h.z;
+
+	cout << k.x << endl;
+	cout << k.y << endl;
+	cout << k.z << endl;
+
+	return 0;
+
+	
 }
